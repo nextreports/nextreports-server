e7803e17e2fe5b0f97b2cbc84141e332233ffc38
Mihai <dpmihai@yahoo.com>
2014-03-24 17:19:22 +0200

    Russian language

--- src/ro/nextreports/server/web/language/LanguageManager.java
+++ src/ro/nextreports/server/web/language/LanguageManager.java
@@ -49,6 +49,10 @@ public class LanguageManager {
 	public static final String COUNTRY_POLISH = "PL";
 	public static final String PROPERTY_NAME_POLISH = LANGUAGE_POLISH + "_" + COUNTRY_POLISH;
 	
+	public static final String LANGUAGE_RUSSIAN = "ru";
+	public static final String COUNTRY_RUSSIAN = "RU";
+	public static final String PROPERTY_NAME_RUSSIAN = LANGUAGE_RUSSIAN + "_" + COUNTRY_RUSSIAN;
+	
 	public static final List<Language> languages = new ArrayList<Language>();			
 	public static final List<String> LANGUAGES = new ArrayList<String>();		
 	
@@ -61,11 +65,13 @@ public class LanguageManager {
 		languages.add(new Language(LANGUAGE_FRENCH, COUNTRY_FRENCH, PROPERTY_NAME_FRENCH));
 		languages.add(new Language(LANGUAGE_ROMANIAN, COUNTRY_ROMANIAN, PROPERTY_NAME_ROMANIAN));
 		languages.add(new Language(LANGUAGE_POLISH, COUNTRY_POLISH, PROPERTY_NAME_POLISH));
+		languages.add(new Language(LANGUAGE_RUSSIAN, COUNTRY_RUSSIAN, PROPERTY_NAME_RUSSIAN));
 		
 		LANGUAGES.add(PROPERTY_NAME_ENGLISH);
 		LANGUAGES.add(PROPERTY_NAME_FRENCH);
 		LANGUAGES.add(PROPERTY_NAME_ROMANIAN);
 		LANGUAGES.add(PROPERTY_NAME_POLISH);
+		LANGUAGES.add(PROPERTY_NAME_RUSSIAN);
 		
 		// try to see if other internatinalization filee where added by hand
 		// must have name like NextServerApplication_<lang>_<country>.properties
